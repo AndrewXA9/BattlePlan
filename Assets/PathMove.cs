a9b095c26a0557a1aeac952e49337723eeb0d2a9
Andrew <andrewxa9@yahoo.com>
2014-11-13 15:30:03 -0800

    movment

---
+++ Assets/PathMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathMove : MonoBehaviour {
+	
+	public bool go = false;
+	public float speed;
+	public Path path;
+	public float progress = 0;
+	
+	void Update () {
+		this.transform.position = Vector3.Lerp(path.nodeVectors[(int)(Mathf.Floor(progress))],path.nodeVectors[(int)(Mathf.Ceil(progress))],progress/(path.nodeVectors.Count-1));
+		progress += Time.deltaTime*speed;
+	}
+	
+}
